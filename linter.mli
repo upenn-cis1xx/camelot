8aa926c2461f333822eea8d74422fe522fbca55a
Vighnesh Vijay <vighnesh@seas.upenn.edu>
2020-04-15 16:19:16 -0400

    ocaml doc setup

--- linter.mli
+++ linter.mli
@@ -1,8 +1,15 @@
+(** The linters interface. You need to provide the following vals
+    -- rules : a list of functions that allow you to generate suggestions
+    -- patterns_of_interest : a function that is passed to the iterator
+       Given a filename and parsetree expression, you can configure this
+       method to write to a database/internal datastructure as a side - effect.
+    -- lint : a function that should produce a set of hints that you store somewhere
+    -- hint : a function that should print the hints you stored in lint
+
+*)
+
 (* List of rules to lint *)
 val rules : (Style.hint list ref -> Style.patternctxt -> unit) list 
-
-(* List of patterns found *)
-val pats : Style.patternctxt list ref
 
 (* Parses patterns of interest - can store these anywhere you like or not at all *)
 val patterns_of_interest : string -> Parsetree.expression -> unit
