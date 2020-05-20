006475ed17217743701bd67ce3fbd567023da73b
Vighnesh Vijay <vighnesh@seas.upenn.edu>
2020-05-19 22:52:46 -0400

    done!

--- examples/verbose.ml
+++ examples/verbose.ml
@@ -9,3 +9,40 @@ let f t = 1 :: [] @ t
 (* Tuple projection bad! *)
 
 let f (t: int * int) = fst t + snd t
+
+
+(* Nested ifs :( - we skip local lets and sequencing to get the actual return type for now *)
+let x = true
+let y = false
+
+let dounit () = ()
+(* No Flagging here - only two levels deep *)
+let f () = if x then (if x then x else y) else y
+let f () = if x then
+    (if x then x
+     else let z = 3 in
+       dounit();
+       (if z = 2 then x else y) )
+  else y
+
+
+(* Nested matched bad as well *)
+
+let f () =
+  let l = [] in
+  begin match l with
+    | [] ->
+      begin match l with
+        | [] ->
+          let z = [] in
+        begin match z with
+          | _ -> true
+        end
+      | _ -> false
+    end
+  | _ -> true
+  end
+  
+    
+          
+      
