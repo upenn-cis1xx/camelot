e42ca967078679fb63b4b801f97e6915c15f8562
Vighnesh Vijay <vighnesh@seas.upenn.edu>
2020-05-21 23:36:38 -0400

    twoke

--- lib/style/lexical.ml
+++ lib/style/lexical.ml
@@ -5,7 +5,8 @@ open Check
 
 
 module LineLength : EXPRCHECK = struct
-     type ctxt = Parsetree.expression_desc Pctxt.pctxt 
+
+  type ctxt = Parsetree.expression_desc Pctxt.pctxt 
   let fix = "indenting to avoid exceeding the line limit"
   let violation = "exceeding the 80 character line limit. Only showing (1) such violation of this kind, although there may be others - fix this and re-run the linter to find them."
     
