7a38952205ce28e174d2b81bc4a45f5c92f0c37e
Jeffrey Xiao <jeffrey.xiao1@gmail.com>
2022-10-18 13:18:14 -0400

    update err message, clean style in camelot

--- lib/style/verbose.ml
+++ lib/style/verbose.ml
@@ -35,7 +35,7 @@ module TupleProj : EXPRCHECK = struct
   let name = "TupleProj", check
 end
 
-(** ----------------------- Checks rules: Nesting if >= 3 levels ------------- *)
+(** ----------------------- Checks rules: Nesting if > 3 levels ------------- *)
 module NestedIf : EXPRCHECK = struct
   type ctxt = Parsetree.expression_desc Pctxt.pctxt
   let fix = "using let statements or helper methods / rethinking logic"
@@ -45,9 +45,9 @@ module NestedIf : EXPRCHECK = struct
       depth = 0 ||
       begin match p with 
         | Pexp_ifthenelse (_, bthen, Some belse) -> 
-          if depth = 1 then true else 
-            find_nesting ((skip_seq_let bthen).pexp_desc) (depth - 1) ||
-            find_nesting ((skip_seq_let belse).pexp_desc) (depth - 1) 
+          depth = 1
+            || find_nesting ((skip_seq_let bthen).pexp_desc) (depth - 1)
+            || find_nesting ((skip_seq_let belse).pexp_desc) (depth - 1) 
         | _ -> false
       end 
     in 
@@ -59,7 +59,7 @@ end
 module NestedMatch : EXPRCHECK = struct
   type ctxt = Parsetree.expression_desc Pctxt.pctxt
   let fix = "using let statements or helper methods / rethinking logic"
-  let violation = "using nested match statements more than three layers deep"
+  let violation = "using nested match statements three or more layers deep"
   let check st (E {location; source; pattern} : ctxt) =
     begin match pattern with
       (* Layer one *)
